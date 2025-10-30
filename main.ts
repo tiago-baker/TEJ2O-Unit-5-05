27787b2265ef59aa446748857da62f42c1eb0d84
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-29 20:35:15 -0400

    fixed lines 30-40

--- main.ts
+++ main.ts
@@ -20,24 +20,22 @@ basic.showIcon(IconNames.Happy)
 // Button A Pressed 
 input.onButtonPressed(Button.A, function () {
 
-        // Green light
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-        neopixelStrip.show()
-        basic.pause(2000)
+    // Green light
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.pause(2000)
 
-        // yellow light
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-        neopixelStrip.show()
-        basic.pause(2000)
+    // yellow light
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(2000)
 
-        // red light
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-        neopixelStrip.show()
-        basic.pause(2000)
-
-    
+    // red light
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.pause(2000)
 
     // Turn off all NeoPixels 
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
