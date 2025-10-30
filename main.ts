a36bd0d7aaeb611f9eee9b0468c4f76d7270ea73
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-29 20:26:24 -0400

    coded and fixed green light

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Tiago Baker
+ * Created on: Oct 2025
+ * This program replicates a traffic light
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+
+// Turn all NeoPixels off
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
+// Button A Pressed 
+input.onButtonPressed(Button.A, function () {
+
+        // Green light
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.pause(2000)
+
+        // yellow light
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.show()
+        basic.pause(2000)
+
+        // red light
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(2000)
+
+    
+
+    // Turn off all NeoPixels 
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+
+})
