0f67a0ce045e75a62d2d4e164661ab14f1065e16
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-06 10:30:58 -0500

    fixed clear strip code

--- main.ts
+++ main.ts
@@ -8,13 +8,7 @@
 // variables
 let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-// Turn all NeoPixels off
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
-basic.showIcon(IconNames.Happy)
+neopixelStrip.clear()
 
 // Button A Pressed 
 input.onButtonPressed(Button.A, function () {
@@ -24,21 +18,17 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
 
     // yellow light
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.clear()
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     neopixelStrip.show()
     basic.pause(2000)
 
     // red light
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.clear()
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
     basic.pause(2000)
 
     // Turn off all NeoPixels 
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
+    neopixelStrip.clear()
 })
