ed533a3fc4251dc0fb51e44b1537a3c3c9fd1f89
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-06 11:04:52 -0500

    moved clear neopixels

--- main.ts
+++ main.ts
@@ -6,46 +6,41 @@
 */
 
 // variables
-let myStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-
-// Turn off all NeoPixels 
-myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-myStrip.show()
+let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 // Button A Pressed 
 input.onButtonPressed(Button.A, function () {
     
+    // Turn all NeoPixels off
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+
     // Green light
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Green))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
     basic.pause(2000)
 
     // yellow light
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
     basic.pause(2000)
 
     // red light
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Red))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
     basic.pause(2000)
 
     // Turn off all NeoPixels 
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
-    myStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+
 })
