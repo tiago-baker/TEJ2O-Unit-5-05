79729871ab5e4e8b00eb63885eb0d8f13aefbad0
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-19 11:05:17 -0500

    delated extra spaces

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Tiago Baker
  * Created on: Oct 2025
- * This program replicates a traffic light
+ * This program replicates a traffic light.
 */
 
 // variables
@@ -17,24 +17,20 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
     basic.showIcon(IconNames.Happy)
-
     // Green light
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
     basic.pause(2000)
-
     // yellow light
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     neopixelStrip.show()
     basic.pause(2000)
-
     // red light
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
     basic.pause(2000)
-
     // Turn off all NeoPixels 
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
