512f12033fa9a00dad7f569f385baf7e4ed45d93
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-19 11:11:32 -0500

    test 17

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 // Button A Pressed 
 input.onButtonPressed(Button.A, function () {
+    
     // Turn all NeoPixels off
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -17,20 +18,24 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
     basic.showIcon(IconNames.Happy)
+
     // Green light
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
     basic.pause(2000)
+
     // yellow light
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     neopixelStrip.show()
     basic.pause(2000)
+
     // red light
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
     basic.pause(2000)
+
     // Turn off all NeoPixels 
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
